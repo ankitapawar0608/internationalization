4ab9cd083ed8241138596a0178b05c2a8f51d184
ankitapawar0608 <kedarpawar775@gmail.com>
2023-12-13 19:26:23 +0530

    basic page ready

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Home from './views/Home/home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>I18n</h1>
+ 
+
+  <Home/>
 );
 
 
