4042c70d52e9db7ac57a3b64d1d16c3318fffb2a
ankitapawar0608 <kedarpawar775@gmail.com>
2023-12-15 17:18:24 +0530

    json file added

--- src/views/Home/home.js
+++ src/views/Home/home.js
@@ -2,16 +2,24 @@ import React from 'react'
 import "./home.css"
 
 function home() {
+
+  const users = 15;
+
+
   return (
     <div>
-        <h1>Welcome to my website</h1>
+      <h1>Welcome to my website</h1>
 
-        <p>
-            I am Ankita Pawar
-        </p>
-        <h3>
-            Have a great day!!!!
-        </h3>
+      <p>
+        I am Ankita Pawar
+      </p>
+      <h3>
+        Have a great day!!!!
+      </h3>
+
+      <p>
+        {users} users are learning react.
+      </p>
     </div>
   )
 }
